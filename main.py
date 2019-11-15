518dce5cb93842886ccdeccbf8162b98508b7737
Sunil Khedekar <sunil.khedekar@tesco.com>
2019-11-15 19:00:12 +0000

    chapter 2

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Daiwik Khedekar',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'November 15, 2019'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Srushti Khedekar',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'November 15, 2019'
     }
 ]
 
